abfea3ec144c22b75eb3b96701a6c6512d0b43de
Raghunadraju <raghu.raju@nimmetry.com>
2018-08-17 13:53:07 +0530

    While loop

--- Loops.py
+++ Loops.py
@@ -3,10 +3,13 @@
 Names = ["RAGHU", 'SRINI', '', 1, 'SHREYA', 'SURYA']
 
 for Names in Names:
+    if Names == "SHREYA":
+        print("Found him "+Names)
+        break  # for loop stops after the condition satisfied
     print('Student Name is {0}'.format(Names))  # Printing all the objects in the list using for loop
 
 a = 2
-for index in range(10):
+for index in range(2):
     a += 2
     print("iterations {0}".format(a))
 
---
+++ While Loop.py
@@ -0,0 +1,6 @@
+# While loop will check the condition before even entering into the loop
+a = 1
+while a < 11:
+    print("{0}".format(a))
+    break
+    a += 1
