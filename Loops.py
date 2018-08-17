78f56ee39a80ae3dcc4e1868a4a6a5c72a258d17
Raghunadraju <raghu.raju@nimmetry.com>
2018-08-17 12:18:04 +0530

    For Loop

---
+++ Loops.py
@@ -0,0 +1,12 @@
+# There are only two main loops in Python (FOR, WHILE)
+
+Names = ["RAGHU", 'SRINI', '', 1, 'SHREYA', 'SURYA']
+
+for Names in Names:
+    print('Student Name is {0}'.format(Names))  # Printing all the objects in the list using for loop
+
+a = 2
+for index in range(10):
+    a += 2
+    print("iterations {0}".format(a))
+
