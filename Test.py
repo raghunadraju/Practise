8617097afc0735955d7259afd8737fb500b4aef5
Raghunadraju <raghu.raju@nimmetry.com>
2018-08-17 09:57:55 +0530

    Initial commit

---
+++ Test.py
@@ -0,0 +1,27 @@
+a = "hello.hi.all".split(".")
+c = 'gm'
+d = 'gn'
+b = 'nice to meet you {0}.hi all {1}'.format(c, d)
+print(a, b)
+
+x = True
+y = None
+z = int(x)
+print(y)
+
+g = 3
+h = 4
+
+var = "bigger" if g > h else "smaller"
+print(var)
+
+no = 10
+if no == 10:
+    print('number is 10')
+else:
+    print("number is not 10")
+
+number = 8
+if number!=9:
+    print('number is truthy')
+
