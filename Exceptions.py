1c89ed8223cfb9610af447e1a6c32d61262cff9b
Raghunadraju <raghu.raju@nimmetry.com>
2018-08-21 09:47:50 +0530

    Exceptions

--- Exceptions.py
+++ Exceptions.py
@@ -13,3 +13,24 @@ except TypeError as error:  # this is to print the error message description
     print(error)
 except Exception:  # Generice exception
     print("Unknown Error")
+
+c=set([1,2,1,3,2]) # which will remove duplicates from the list
+print(c)
+# Get three test score
+round1 = int(input("Enter score for round 1: "))
+
+round2 = int(input("Enter score for round 2: "))
+
+round3 = int(input("Enter score for round 3: "))
+
+# Calculate the average
+average = (round1 + round2 + round3) / 3
+
+# Print out the test score
+print("the average score is: ", average)
+
+import string
+from random import *
+characters = string.ascii_letters + string.punctuation  + string.digits
+password =  "".join(choice(characters) for x in range(randint(8, 16)))
+print(password)
